1ce8bb0d1f0ad621b8dcda083f57ec1e3a879fa0
Arjun <arjun.aletty@gmail.com>
2013-08-06 13:36:17 -0700

    added proper alert fadout

--- utils/notify.js
+++ utils/notify.js
@@ -2,8 +2,8 @@ var io = require('socket.io-client');
 
 //Keely if it complains when you do localhost comment out the herokuapp line and uncomment the localhost
 
-var socket = io.connect('http://shwastinator.herokuapp.com/notify');
-// var socket = io.connect('http://localhost:3000/notify');
+// var socket = io.connect('http://shwastinator.herokuapp.com/notify');
+var socket = io.connect('http://localhost:3000/notify');
 
 exports.push = function (user, message, type) {
   //user: name of user to notify normally req.session.user.name
